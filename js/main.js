3589a4005d35642f0a456b17d3013ab0e3204646
DIEGO <cg3017311@LAB203M16.IFSP>
2023-05-18 22:31:16 -0300

    there's an exercise to be done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const openModal = (idModal) => {
     const divModal = document.querySelector(idModal)
-    divModal.style.display = "flex"
+    divModal.style.display = "flex";
 }
 
 const closeModal = (idModal) => {
     const divModal = document.querySelector(idModal)
-    divModal.style.display = "none"
+    divModal.style.display = "none";s
 }
 
 const handleModalClose = (event) => {
@@ -14,5 +14,75 @@ const handleModalClose = (event) => {
     }
 }
 
-const modal = document.querySelector(".modal")
-modal.addEventListener("click", handleModalClose)
+const fetchData = async () => {
+    try{
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=LH07N0TNNG27SA5W`)
+        const data = await response.json();
+        return data;
+    } catch(error){
+        alert(error)
+    }
+}
+
+const handleAddTicker = (event) => {
+    event.preventDefault() 
+    const ticker = event.target.ticker.value;
+    const data = fetchData()
+    let price;
+   
+        price = data["Global Quote"]["05. price"];
+        if(price){
+            const newTicker = `<div class="ticker">
+            <button class="btn-close--ticker" onclick="removeTicker(event)">x</button>
+            <h2>${ticker}</h2>
+            <p>${price}</p>
+            <button class='btn-update--ticker' onclick="updateTicker(event)">Update </button>`;
+            addTickersCloseEvent()
+            const tickerList = document.querySelector('#tickers-list');
+            tickerList.innerHTML += newTicker 
+        }else{
+            alert(`Ticker ${ticker} não encontrado!`)
+        }
+}
+
+const handleTickerMouseEnter = (event) => {
+    const ticker = event.target;
+    const btnClose = ticker.querySelector('.btn-close--ticker');
+    btnClose.style.display = 'block';
+}
+
+const handleTickerMouseLeave = (event) => {
+    const ticker = event.target;
+    const btnClose = ticker.querySelector('.btn-close--ticker');
+    btnClose.style.display = 'none';
+}
+
+const removeTicker = (event) => {
+    const btnClose = event.target;
+    const ticker = btnClose.closest('.ticker');
+    ticker.remove();
+}
+
+const modal = document.querySelector(".modal");
+modal.addEventListener("click", handleModalClose);
+
+const addTickersCloseEvent = (event) => {
+    const ticker = document.querySelectorAll('.ticker');
+    ticker.forEach((ticker) => {
+        ticker.addEventListener('mouseenter', handleTickerMouseEnter);
+        ticker.addEventListener('mouseleave', handleTickerMouseEnter);
+    })
+}
+
+const updateTicker = (event) => {
+    const data = fetchData();
+    const ticker = event.target.ticker.value;
+    const btnClose = event.target;
+    const  ticker = btnClose.closest('.ticker');
+    let price = data["Global Quote"]["05. price"];
+    ticker.querySelector('p').innerHTML = price;
+}
+
+console.log(fetchData())
+
+addTickersCloseEvent()
